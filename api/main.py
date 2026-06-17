a1fa359918fea92289858b367f97be4f1569f283
jesriel33 <jesriel33@gmail.com>
2026-06-17 19:50:42 +0800

    rev_2

--- api/main.py
+++ api/main.py
@@ -536,4 +536,4 @@ def view_image(filename: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)1git s
